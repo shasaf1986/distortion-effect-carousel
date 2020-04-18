bed284156fd3d583924821b24de784d40dbf62b7
shasaf1986 <asaf.shmueli@venn.city>
2020-04-19 01:33:26 +0300

    update packages

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { withKnobs } from '@storybook/addon-knobs';
 import App from '../src/app';
 
-addDecorator(storyFn => <App>{storyFn()}</App>);
+addDecorator((storyFn) => <App>{storyFn()}</App>);
 addDecorator(withKnobs);
 
 configure(require.context('../src', true, /\.stories\.tsx$/), module);
